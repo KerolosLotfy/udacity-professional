31066683c69884203faa624a00b339d9fcce066d
Kerolos Lotfy <100154566+KerolosLotfy@users.noreply.github.com>
2022-07-29 14:00:27 +0200

    updates
    add projects

---
+++ landing page/README .md
@@ -0,0 +1,28 @@
+# Frist Project
+
+## Project Name :-
+
+landing page.
+
+## Project Description :-
+
+landing page.
+
+## Additions :-
+
+- add logo in page header.
+- add button to scroll up.
+- add section number form fontAwesome
+
+## Edits :-
+
+- edit on page header.
+- edit on active section.
+- edit on nav menu (by media query).
+
+## New information I learned from this project :-
+
+- how to use `ele.getBoundingClientRect()`.
+- how to use `window.scrollTo()` and `window.scrollIntoView()`.
+- how to add My Projects on **Github**.
+- learn writing with **Markdown**.
---
+++ landing page/js/app.js
@@ -0,0 +1,140 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+// add (sections) varible to select all sections.
+const sections = document.querySelectorAll("section");
+
+//  add (navBarList) varible to select nav list.
+const navBarList = document.querySelector("#navbar__list");
+
+// add scroll up button
+const btnUp = document.querySelector('#btnUp');
+
+// edit section number (i)
+let secNum = document.querySelectorAll(".secNum")
+
+
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+function buildNav() {
+    for (let i = 0; i < sections.length; i++) {
+    let listItem = document.createElement("li");
+    let menuLink = document.createElement("a");
+    menuLink.className = "menu__link"
+    menuLink.setAttribute("href", `#${sections[i].id}`);
+    menuLink.innerText = sections[i].dataset.nav;
+    navBarList.appendChild(listItem)
+    listItem.appendChild(menuLink);
+    };
+};
+
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+buildNav();
+    
+
+// Add class 'active' to section when near top of viewport
+window.onscroll = function () {
+    for (let section of sections) {
+        let sectionTop = section.getBoundingClientRect().top;
+        let activeLink = document.querySelector(`a[href = "#${section.id}"]`);
+        if (sectionTop >= 0 && sectionTop < 300) {
+            section.classList.add("your-active-class");
+            activeLink.classList.add("active");
+            
+        } else {
+            section.classList.remove("your-active-class");
+            activeLink.classList.remove("active");
+        }
+    };
+
+    // to show or hide scroll up button
+    if (window.scrollY >= 200) {
+        btnUp.style.display = "block";
+    } else {
+        btnUp.style.display = "none";
+    }
+    
+};
+
+
+
+
+
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// add click event for scroll up button
+btnUp.addEventListener('click', function () {
+    window.scrollTo({
+        behavior: 'smooth',
+        top: 0,
+    })
+});
+
+// Build menu
+
+// Scroll to section on link click
+
+sections.forEach(section => {
+    let menuLink = document.querySelector(`a[href = "#${section.id}"]`);
+    menuLink.addEventListener("click", function (event) {
+        event.preventDefault();        
+        section.scrollIntoView({ behavior: "smooth" });
+    });
+});
+
+
+// Set sections as active
+
+// edit postion  for even children 
+secNum.forEach((el, i) => {
+    if ((i+1) % 2 === 0) { 
+        secNum[i].style.right = "87%";
+    }
+})
